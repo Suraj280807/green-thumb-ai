72938afe115c0054597624f02508f32688fa948e
Suraj Patil <surajpatil2882007@example.com>
2026-05-16 15:28:54 +0530

    Add capacitor and android config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.greenthumbai.app',
+  appName: 'Green Thumb AI',
+  webDir: 'dist'
+};
+
+export default config;
